3f5613928cdfc02c4b2ff57fcef367438843f0b5
Owen Taylor <Owentaylor60@yahoo.com>
2022-10-10 17:02:48 -0400

    comment model/routes created, pizza model updated, pizza list front-end done

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,7 +3,12 @@ const { Pizza } = require('../models')
 const pizzaController = {
 
     getAllPizza(req,res) {
-     Pizza.find({})
+     Pizza.find({}).populate({
+        path: 'comments',
+        select: '-__v'
+     })
+     .select('-__v')
+     .sort({_id: -1})
      .then(dbPizzaData => res.json(dbPizzaData))
      .catch(err => {
         console.log(err)
